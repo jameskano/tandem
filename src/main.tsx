2a538e391b1f34a70fbfb28cc6ec5f6a990d42df
Jaime <jaimecanovasdesign@gmail.com>
2026-01-20 16:44:30 +0100

    Fix basic errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import QueryProvider from './shared/providers/QueryProvider'
-import ThemeProvider from './shared/providers/ThemeProvider'
-import { router } from './app/routes'
 import './styles/tailwind.css'
-
-// Initialize Sentry (optional)
-if (import.meta.env.VITE_SENTRY_DSN) {
-  import('./app/sentry').then(({ initSentry }) => {
-    initSentry()
-  })
-}
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryProvider>
-      <ThemeProvider>
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </QueryProvider>
+    <App />
   </React.StrictMode>,
 )
